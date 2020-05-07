140d6d252d5d9e19100a6c50415006e5e7a498de
BraianVargas <63650465+BraianVargas@users.noreply.github.com>
2020-05-07 01:05:16 -0300

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,144 @@
+from ClaseConjunto import Conjunto
+
+import gc
+
+import os
+
+if __name__=='__main__':
+    def opcion1():
+        c1=Conjunto(0)
+        c2=Conjunto(0)
+        print("")
+        print("*** UNION DE CONJUNTOS ***")
+        print("*** CONJUNTO 1 ***")
+        lon=input("Igrese la cantidad de elementos del conjunto: ")
+        try:
+            lon=int(lon)
+            c1.AgregaCjto(lon)
+            os.system('cls')
+        except ValueError:
+            print("Debe ingresar un numero entero.")
+        print("*** CONJUNTO 2 ***")
+        lon=input("Igrese la cantidad de elementos del conjunto: ")
+        try:
+            lon=int(lon)
+            c2.AgregaCjto(lon)
+            os.system('cls')
+        except ValueError:
+            print("Debe ingresar un numero entero.")
+
+        print("Conjunto 1:")
+        c1.Mostrar()
+        print("Conjunto 2:")
+        c2.Mostrar()
+
+
+        if(c1!=None and c2!=None):
+            c3=c1+c2
+            print("La suma de los conjuntos es: ")
+            c3.Mostrar()
+            gc.collect()
+            del c1,c2
+        else: print("No se ha podido realizar la operacion.")
+
+    def opcion2():        
+        c1=Conjunto(0)
+        c2=Conjunto(0)
+        print("")
+        print("*** DIFERENCIA DE CONJUNTOS ***")
+        print("*** CONJUNTO 1 ***")
+        lon=input("Igrese la cantidad de elementos del conjunto: ")
+        try:
+            lon=int(lon)
+            c1.AgregaCjto(lon)
+            os.system('cls')
+        except ValueError:
+            print("Debe ingresar un numero entero.")
+        print("*** CONJUNTO 2 ***")
+        lon=input("Igrese la cantidad de elementos del conjunto: ")
+        try:
+            lon=int(lon)
+            c2.AgregaCjto(lon)
+            os.system('cls')
+        except ValueError:
+            print("Debe ingresar un numero entero.")
+
+        print("Conjunto 1:")
+        c1.Mostrar()
+        print("Conjunto 2:")
+        c2.Mostrar()
+
+
+        if(c1!=None and c2!=None):
+            c3=c1-c2
+            print("La suma de los conjuntos es: ")
+            c3.Mostrar()
+            gc.collect()
+            del c1,c2
+        else: print("No se ha podido realizar la operacion.")
+
+    def opcion3():        
+        print("")
+        print("*** IGUALDAD DE CONJUNTOS ***")
+        print("*** CONJUNTO 1 ***")
+        lon=input("Igrese la cantidad de elementos del conjunto: ")
+        c1=Conjunto(0)
+        try:
+            lon=int(lon)
+            c1.AgregaCjto(lon)
+            os.system('cls')
+        except ValueError:
+            print("Debe ingresar un numero entero.")
+        print("*** CONJUNTO 2 ***")
+        lon=input("Igrese la cantidad de elementos del conjunto: ")
+        c2=Conjunto(0)
+        try:
+            lon=int(lon)
+            c2.AgregaCjto(lon)
+            
+            os.system('cls')
+        except ValueError:
+            print("Debe ingresar un numero entero.")
+
+        print("Conjunto 1:")
+        c1.Mostrar()
+        print("Conjunto 2:")
+        c2.Mostrar()
+
+
+        if(c1!=None and c2!=None):
+            c3 = c1==c2
+            print("La igualdad de los conjuntos es: {}".format(c3))
+            gc.collect()
+            del c1,c2
+        else: print("No se ha podido realizar la operacion.")
+
+
+    def opcion4():
+        pass
+
+    
+
+    switcher ={
+        1: opcion1,
+        2: opcion2,
+        3: opcion3,
+        4: opcion4
+    }
+
+    def switch(argument):
+        func=switcher.get(argument,lambda:print("Opcion Incorrecta"))
+        func()
+
+    ban=False
+    while not ban:
+        print("")
+        print("1. Union de conjuntos.")
+        print("2. Diferencia de conjuntos")
+        print("3. Comparar igualdad de conjuntos.")
+        print("4. Salir")
+
+        opcion=input("Ingrese una opción.")
+        opcion=int(opcion)
+        switch(opcion)
+        ban=opcion==4
