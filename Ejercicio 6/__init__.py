a366ef01c516404377b085cd20ce4af5461fb17e
BraianVargas <63650465+BraianVargas@users.noreply.github.com>
2020-05-07 05:13:02 -0300

    Update __init__.py

--- Ejercicio 6/__init__.py
+++ Ejercicio 6/__init__.py
@@ -5,6 +5,8 @@ import gc
 import os
 
 if __name__=='__main__':
+    bic=False
+    
     print("Ingrese Fecha.")
 
     dia=input("Dia: ")
@@ -14,9 +16,18 @@ if __name__=='__main__':
         dia=int(dia)
         mes=int(mes)
         año=int(año)
+
+        if((año%400==0)or(año%100!=0 and año%4==0)):
+            listames=[31,29,31,30,31,30,31,31,30,31,30,31]
+        else:
+            listames=[31,28,31,30,31,30,31,31,30,31,30,31]
+        if(dia<=listames[mes-1] and dia > 0):
+            bic=False
+        else:
+            print("La fecha ingresada no es válida") 
+            bic=True
     except ValueError:
         print("Debe ingresar Numeros enteros")
-
 
     def opcion1():
         print("*********** SUMAR HORAS ********")
@@ -27,7 +38,7 @@ if __name__=='__main__':
             minutos=int(input("Ingrese Minutos: "))
             segundos=int(input("Ingrese Segundos: "))
             a=FechaHora(dia, mes, año, hora, minutos, segundos)
-
+            
             print("**** Hora 2 ****")
             hora1=int(input("Ingrese Hora: "))
             minutos1=int(input("Ingrese Minutos: "))
@@ -123,14 +134,18 @@ if __name__=='__main__':
         func=switcher.get(argument,lambda:print("Opcion Incorrecta"))
         func()
 
-    ban=False
-    while not ban:
-        print("1. Sumar Hora")
-        print("2. Restar Hora")
-        print("3. Distinguir entre dos horas cuál es mayor")
-        print("4. Salir")
 
-        opcion=input("Ingrese su opcion: ")
-        opcion=int(opcion)
-        switch(opcion)
-        ban = int(opcion) == 4
+    if bic== False:
+        ban = False
+        while not ban:
+            print("1. Sumar Hora")
+            print("2. Restar Hora")
+            print("3. Distinguir entre dos horas cuál es mayor")
+            print("4. Salir")
+
+            opcion=input("Ingrese su opcion: ")
+            opcion=int(opcion)
+            switch(opcion)
+            ban = int(opcion) == 4
+    
+    input("Presione ENTER para continuar")
