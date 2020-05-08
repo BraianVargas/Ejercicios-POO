3d6251493603856a7a30ca614162efc74ff86a77
BraianVargas <63650465+BraianVargas@users.noreply.github.com>
2020-05-08 16:56:48 -0300

    Update __init__.py

--- Ejercicio 8/__init__.py
+++ Ejercicio 8/__init__.py
@@ -1,123 +1,115 @@
-from ClaseConjunto import Conjunto
+from ClaseFechaHora import FechaHora
 
 import gc
 
 import os
 
 if __name__=='__main__':
+    bic=False
+
+    print("**** Hora y Fecha1 ****")
+    dia=int(input("Dia: "))
+    mes=int(input("Mes: "))
+    año=int(input("Año: "))
+    hora=int(input("Hora: "))
+    minuto=int(input("Minuto: "))
+    segundo=int(input("Segundo: "))
+
+    r=FechaHora(dia,mes,año,hora,minuto,segundo)
+    r.Mostrar()
+    
+    try:
+        dia=int(dia)
+        mes=int(mes)
+        año=int(año)
+
+        if((año%400==0)or(año%100!=0 and año%4==0)):
+            listames=[31,29,31,30,31,30,31,31,30,31,30,31]
+        else:
+            listames=[31,28,31,30,31,30,31,31,30,31,30,31]
+        if(dia<=listames[mes-1] and dia > 0):
+            bic=False
+        else:
+            print("La fecha ingresada no es válida") 
+            bic=True
+    except ValueError:
+        print("Debe ingresar Numeros enteros")
+
     def opcion1():
-        c1=Conjunto()
-        c2=Conjunto()
+        print("*********** SUMAR HORAS ********")
         print("")
-        print("*** UNION DE CONJUNTOS ***")
-        print("*** CONJUNTO 1 ***")
-        lon=input("Igrese la cantidad de elementos del conjunto: ")
-        try:
-            lon=int(lon)
-            c1.AgregaCjto(lon)
-            os.system('cls')
+        try:            
+            print("**** Hora 2 ****")
+            hora1=int(input("Ingrese Hora: "))
+            #minutos1=int(input("Ingrese Minutos: "))
+            #segundos1=int(input("Ingrese Segundos: "))
+            #b=FechaHora(dia, mes, año, hora1, minutos1, segundos1)
+            
+            c=r+hora1
+
+            print("La suma de las horas es: ")
+            r.Mostrar()
+
         except ValueError:
-            print("Debe ingresar un numero entero.")
-        print("*** CONJUNTO 2 ***")
-        lon=input("Igrese la cantidad de elementos del conjunto: ")
-        try:
-            lon=int(lon)
-            c2.AgregaCjto(lon)
-            os.system('cls')
-        except ValueError:
-            print("Debe ingresar un numero entero.")
+            print("Debe ingresar Numeros enteros")
 
-        print("Conjunto 1:")
-        c1.Mostrar()
-        print("Conjunto 2:")
-        c2.Mostrar()
+        gc.collect()
+        input("Enter para continuar...")
+        os.system('cls')
 
-
-        if(c1!=None and c2!=None):
-            c3 = c1+c2
-            print("La union de los conjuntos es: ",c3)
-            gc.collect()
-        else: print("No se ha podido realizar la operacion.")
-        del c1,c2, c3
-
-    def opcion2():        
-        c1=Conjunto()
-        c2=Conjunto()
+    def opcion2():
+        print("*********** RESTAR HORAS ********")
         print("")
-        print("*** DIFERENCIA DE CONJUNTOS ***")
-        print("*** CONJUNTO 1 ***")
-        lon=input("Igrese la cantidad de elementos del conjunto: ")
         try:
-            lon=int(lon)
-            c1.AgregaCjto(lon)
-            os.system('cls')
-        except ValueError:
-            print("Debe ingresar un numero entero.")
-        print("*** CONJUNTO 2 ***")
-        lon=input("Igrese la cantidad de elementos del conjunto: ")
-        try:
-            lon=int(lon)
-            c2.AgregaCjto(lon)
-            os.system('cls')
-        except ValueError:
-            print("Debe ingresar un numero entero.")
-
-        print("Conjunto 1:")
-        c1.Mostrar()
-        print("Conjunto 2:")
-        c2.Mostrar()
-
-
-        if(c1!=None and c2!=None):
-            c3=c1-c2
-            print("La suma de los conjuntos es: ",c3)
-            gc.collect()
-        else: print("No se ha podido realizar la operacion.")
-        del c1,c2,c3
-
-    def opcion3():        
-        print("")
-        print("*** IGUALDAD DE CONJUNTOS ***")
-        print("*** CONJUNTO 1 ***")
-        lon=input("Igrese la cantidad de elementos del conjunto: ")
-        c1=Conjunto()
-        try:
-            lon=int(lon)
-            c1.AgregaCjto(lon)
-            os.system('cls')
-        except ValueError:
-            print("Debe ingresar un numero entero.")
-        print("*** CONJUNTO 2 ***")
-        lon=input("Igrese la cantidad de elementos del conjunto: ")
-        c2=Conjunto()
-        try:
-            lon=int(lon)
-            c2.AgregaCjto(lon)
-
-            os.system('cls')
-        except ValueError:
-            print("Debe ingresar un numero entero.")
-
-        print("Conjunto 1:")
-        c1.Mostrar()
-        print("Conjunto 2:")
-        c2.Mostrar()
-
-        
-        if(c1!=None and c2!=None):
-            if(c1==c2):
-                print("Los conjuntos son iguales")
+            print("**** Hora 2 ****")
+            hora1=int(input("Ingrese Hora: "))
+            #minutos1=int(input("Ingrese Minutos: "))
+            #segundos1=int(input("Ingrese Segundos: "))
+            #b=FechaHora(dia, mes, año, hora1, minutos1, segundos1)
+            
+            c=r-hora1
+            
+            if(c!=-1):
+                print("La Resta de las horas es: ")
+                r.Mostrar()
             else:
-                print("Los conjuntos no son iguales")
-            gc.collect()
+                print("No se ha podido relizar la resta. Intente Nuevamente")
 
-            del c1,c2
-        else: print("No se ha podido realizar la operacion.")
-        input("")
+        except ValueError:
+            print("Debe ingresar Numeros enteros")
+
+        gc.collect()   
+        input("Enter para continuar...")
+        os.system('cls')
+
+    def opcion3():
+        print("*********** COMPARACION DE HORAS ********")
+        print("")
+        try:
+            print("**** Hora 2 ****")
+            print("")
+            hora1=int(input("Ingrese Hora: "))
+            #minutos1=int(input("Ingrese Minutos: "))
+            #segundos1=int(input("Ingrese Segundos: "))
+            #b=FechaHora(dia, mes, año, hora1, minutos1, segundos1)
+
+            c=r>hora1
+            if(c==1):
+                print("(Hora 1) es mayor: ",hora)
+            elif(c==0):
+                print("(Hora 2) es mayor: ",hora1)
+            else:
+                print("Las horas ingresadas son iguales")    
+        except ValueError:
+            print("Debe ingresar Numeros enteros")
+
+        gc.collect()   
+        input("Enter para continuar...")
+        os.system('cls')
+
 
     def opcion4():
         pass
-
 
     switcher ={
         1: opcion1,
@@ -125,20 +117,22 @@ if __name__=='__main__':
         3: opcion3,
         4: opcion4
     }
-
     def switch(argument):
         func=switcher.get(argument,lambda:print("Opcion Incorrecta"))
         func()
 
-    ban=False
-    while not ban:
-        print("")
-        print("1. Union de conjuntos.")
-        print("2. Diferencia de conjuntos")
-        print("3. Comparar igualdad de conjuntos.")
-        print("4. Salir")
 
-        opcion=input("Ingrese una opción.")
-        opcion=int(opcion)
-        switch(opcion)
-        ban=opcion==4
+    if bic== False:
+        ban = False
+        while not ban:
+            print("1. Sumar Hora")
+            print("2. Restar Hora")
+            print("3. Distinguir entre dos horas cuál es mayor")
+            print("4. Salir")
+
+            opcion=input("Ingrese su opcion: ")
+            opcion=int(opcion)
+            switch(opcion)
+            ban = int(opcion) == 4
+    
+    input("Presione ENTER para continuar")
